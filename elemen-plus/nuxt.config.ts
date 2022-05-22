5e4297f9e913a3b2b7ae742239421fc7e97cf51f
Huakun Shen <huakun.shen@inossem.com>
2022-05-22 02:28:19 -0400

    update element ui setup

--- elemen-plus/nuxt.config.ts
+++ elemen-plus/nuxt.config.ts
@@ -1,6 +1,13 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
+
+const lifecycle = process.env.npm_lifecycle_event;
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	css: ['element-plus/dist/index.css']
-})
+  css: ['element-plus/dist/index.css'],
+  // build: {transpile: ['element-plus']}
+  build: {
+    transpile: lifecycle === 'build' || lifecycle === 'generate' ? ['element-plus'] : [],
+  },
+  // ssr: false
+});
