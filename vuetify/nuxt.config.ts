5ed247163e3c704116c69e5f277213aa151527f3
Huakun Shen <huakun.shen@huakunshen.com>
2022-06-16 16:06:14 -0400

    Fix nuxt vuetify error

--- vuetify/nuxt.config.ts
+++ vuetify/nuxt.config.ts
@@ -1,5 +1,4 @@
 import { defineNuxtConfig } from "nuxt";
-const { VuetifyPlugin } = require("webpack-plugin-vuetify");
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
