fa598890409326b4ed164a510559fcc983b13c19
Huakun Shen <huakun.shen@inossem.com>
2022-06-16 15:41:20 -0400

    init vuetify nuxt app

---
+++ antd/antd.js
@@ -0,0 +1,22 @@
+import { Breadcrumb, BreadcrumbItem, Button } from 'ant-design-vue/lib';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  // nuxtApp.vueApp.use(Button);
+  // nuxtApp.vueApp.use(Breadcrumb);
+  // nuxtApp.vueApp.use(BreadcrumbItem);
+  // nuxtApp.vueApp.component('AButton', Button)
+  // nuxtApp.vueApp.component('ABreadcrumb', Breadcrumb)
+  // nuxtApp.vueApp.component('ABreadcrumbItem', BreadcrumbItem)
+
+});
+
+
+
+/**
+ * Import Everything At Once (Large Bundle Size)
+ */
+// import Antd from 'ant-design-vue/lib';
+
+// export default defineNuxtPlugin((nuxtApp) => {
+// 	nuxtApp.vueApp.use(Antd);
+// });
