716a614e65427068f50dc508536dac463430bb01
Huakun Shen <huakun.shen@inossem.com>
2022-06-16 16:05:24 -0400

    Vuetify Implemented

--- vuetify/app.vue
+++ vuetify/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n" cols="12" class="pa-2" sm="4">
+          <v-alert type="success">Vuetify Alert</v-alert>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
