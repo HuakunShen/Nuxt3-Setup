2d00559cceef0df0c701d44557c334c43c1f638a
Huakun Shen <huakun.shen@huakunshen.com>
2022-12-23 08:42:53 -0500

    Update tailwind to nuxt stable 3.0.0 release

--- tailwind/nuxt.config.ts
+++ tailwind/nuxt.config.ts
@@ -1,14 +1,4 @@
-import { defineNuxtConfig } from 'nuxt';
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config.js',
-    exposeConfig: false,
-    config: {},
-    injectPosition: 0,
-    viewer: true,
-  }
-});
+	modules: ['@nuxtjs/tailwindcss']
+})
