525d8baaa4ebb8b2917b83cb9d0c9cbaa1415d52
Huakun Shen <huakun.shen@huakunshen.com>
2023-04-07 05:30:40 -0400

    Get most plugins working

--- full-features/nuxt.config.ts
+++ full-features/nuxt.config.ts
@@ -1,16 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    "@pinia/nuxt",
     "@nuxtjs/tailwindcss",
     "@nuxtjs/i18n",
-    // "@nuxtjs/eslint-module",
+    "@nuxtjs/eslint-module",
     "@nuxtjs/device",
     // '@nuxtjs/apollo',
     "@nuxtjs/robots",
-    "nuxt-icon"
+    "nuxt-icon",
+    "@nuxtjs/color-mode"
   ],
-  i18n: {},
+  // apollo: {},
+  i18n: {
+    // add `vueI18n` option to `@nuxtjs/i18n` module options
+    vueI18n: {
+      legacy: false,
+      locale: "en",
+      messages: {
+        en: {
+          welcome: "Welcome",
+        },
+        fr: {
+          welcome: "Bienvenue",
+        },
+      },
+    },
+  },
   robots: {},
+  // apollo: {
+  //   clientConfigs: {
+  //     default: {
+  //       httpEndpoint: 'https://api.spacex.land/graphql'
+  //     },
+  //   },
+  // },
   // eslint: {},
   //   modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss", "@nuxtjs/eslint-module", '@nuxtjs/apollo'],
   //   apollo: {
@@ -20,4 +44,4 @@ export default defineNuxtConfig({
   //       }
   //     }
   //   }
-});
+})
